42562d124d9b0785b9342c533c4ba2ee0c7ce038
Scott Walker <walkesco@seawolf.sonoma.edu>
2013-10-31 18:08:55 -0700

    Updated Files

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 # VARIABLES
 
-dir=~/dot_files/
-target=~
+dir=~/dot_files
 settingsList=(.zshrc .vimrc)
 
 # Copy the settings files in the home directory
+mkdir $HOME/dot_files_backup 2> /dev/null
 for name in ${settingsList[@]}
 do
     echo $name
-    ln -s $target/$name $dir
+    cp $HOME/$name $HOME/dot_files_backup/ 2> /dev/null
+    ln -s $HOME/dot_files/$name $HOME/$name 2> /dev/null
 done
